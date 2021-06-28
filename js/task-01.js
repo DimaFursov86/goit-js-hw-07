30f607e9e4f5ae5afe9b23f6c87857da9882223f
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-06-29 02:57:46 +0300

    added task-1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+
+const allCategoriesListEl = document.querySelectorAll('.item');
+
+console.log(`В списке ${allCategoriesListEl.length} категории.`);
+
+allCategoriesListEl.forEach((element) =>
+    (console.log(`Категория: ${element.firstElementChild.textContent}`),
+    console.log(`Количество элементов: ${element.childNodes[3].children.length}`)));
+
+
+
+
+
