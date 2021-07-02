0bd6c544a4cfcd2da5b25e96b4c3e1b6cdc1071f
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-07-03 02:03:27 +0300

    added task-6

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener("blur", onInputValidation);
+
+function onInputValidation(event) {
+    const datalength = event.currentTarget.dataset.length;
+    const inputLength = event.currentTarget.value.length;
+   
+    if (Number(datalength) === inputLength) {
+        (inputEl.classList.add("valid")) || (inputEl.className = "valid");
+      
+    } 
+        else inputEl.classList.add("invalid") || (inputEl.className = "invalid");
+      
+};
+
