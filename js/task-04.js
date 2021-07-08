5620648c4150e459827451f74deab55bd0aaaa35
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-07-08 10:06:06 +0300

    correction-3

--- js/task-04.js
+++ js/task-04.js
@@ -32,3 +32,30 @@ CounterPlugin.prototype._bindEvents = function () {
 };
 
 const counter = new CounterPlugin();
+
+
+
+// let counterValue = 0;
+
+// // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+
+// const decrementBtnEl = document.querySelector(
+//   '#counter button[data-action="decrement"]',
+// );
+// const incrementBtnEl = document.querySelector(
+//   '#counter button[data-action="increment"]',
+// );
+// const counterEl = document.querySelector('#counter #value')
+
+// const onClickDecrement = () => {
+// 	counterValue--;
+// 	counterEl.textContent = counterValue;
+// };
+// const onClickIncrement = () => {
+// 	counterValue++;
+// 	counterEl.textContent = counterValue;
+// };
+
+// decrementBtnEl.addEventListener('click', onClickDecrement);
+
+// incrementBtnEl.addEventListener('click', onClickIncrement);
